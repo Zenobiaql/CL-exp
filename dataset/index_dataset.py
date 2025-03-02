27f480dc95a30e6693258c387a35f6a1a6ae6252
Zenobiaql <zhangqilin8879@mail.ustc.edu.com>
2025-03-02 09:36:12 +0800

    debug

--- dataset/index_dataset.py
+++ dataset/index_dataset.py
@@ -21,7 +21,7 @@ class SimplerDataset(Dataset):
                
         action_file  = self.data_dir / 'action.npy'
         actions = np.load(action_file)
-        self.indx = [i for i in range(40)]
+        self.indx = [i for i in range(len(actions)//2)]
         
                       
     def __len__(self):
