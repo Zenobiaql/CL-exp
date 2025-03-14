d4b3c165084d262a3f8c79eef4d01fb4559a3156
Zenobiaql <zhangqilin8879@mail.ustc.edu.com>
2025-03-14 09:24:03 +0800

    pissa config

--- scripts/sequential_finetune.py
+++ scripts/sequential_finetune.py
@@ -331,7 +331,7 @@ def finetune(cfg: FinetuneConfig)->None:
 
     if cfg.use_lora:
         lora_config = LoraConfig(
-            init_lora_weights="olora",
+            init_lora_weights="pissa",
             r=cfg.lora_rank,
             lora_alpha=min(cfg.lora_rank, 16),
             lora_dropout=cfg.lora_dropout,
