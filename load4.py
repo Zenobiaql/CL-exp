9c5472d4277ac9b8b5bf17b1565ecfde2bbd4085
Zenobiaql <zhangqilin8879@mail.ustc.edu.com>
2025-03-24 22:19:54 +0800

    add pissa config

--- load4.py
+++ load4.py
@@ -43,9 +43,9 @@ import shutil
 # load configs
 class LoadConfigs:
     def __init__(self):
-        self.vla_path = "/mnt/data-qilin/mr_sqft/OLoRA/google_robot_close_top_drawer/merged/epoch24"
-        self.adapter_path = "/mnt/data-qilin/mr_sqft/OLoRA/google_robot_open_bottom_drawer/raw_adapter/epoch24"
-        self.ckpt_path = "/mnt/data-qilin/mr_sqft/OLoRA/google_robot_open_bottom_drawer/merged/epoch24"
+        self.vla_path = "/mnt/data-qilin/mr_sqft/PiSSA/google_robot_close_top_drawer/merged/epoch24"
+        self.adapter_path = "/mnt/data-qilin/mr_sqft/PiSSA/google_robot_open_bottom_drawer/raw_adapter/epoch24"
+        self.ckpt_path = "/mnt/data-qilin/mr_sqft/PiSSA/google_robot_open_bottom_drawer/merged/epoch24"
 
 # load model       
 def load_model():
